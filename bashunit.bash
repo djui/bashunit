d14a5989ceaf19986ff95e106c355bf743c8beec
Uwe Dauernheim <uwe@dauernheim.net>
2013-01-22 22:18:39 +0100

    Update documentation

--- bashunit.bash
+++ bashunit.bash
@@ -1,25 +1,82 @@
 #!/bin/bash
 
 ########################################################################
-# DOCUMENTATION
+# USAGE
 ########################################################################
 
-# A functions starting with 'test' will be executed.
+# Functions starting with 'test' will be automatically evaluated.
 #
-# 1) Write testcases
+# 1. Write test cases
 #
 #     :
-#     testHelloWorld() {
-#         assertEqual "foo" "foo"
+#     testEcho() {
+#         assertEqual "$(echo foo)" "foo"
+#         assertReturn "$(echo foo)" 0
 #     }
 #     :
 #
-# 2) Include this script at the end of your test script
+# 2. Include this script at the end of your test script
 #
 #    :
 #    source $(dirname $0)/bashunit.bash
+#    # eof
 #
-# Dependencies are: Bash (BASH_LINENO), Shell colors.
+# 3. Run test suite
+#
+#    $ ./test_example
+#    testEcho:4:Passed
+#    testEcho:5:Passed
+#    Done. 2 passed. 0 failed. 0 skipped.
+
+########################################################################
+# DEPENDENCIES
+########################################################################
+
+# * Bash (BASH_LINENO)
+# * Shell colours
+
+########################################################################
+# API
+########################################################################
+
+# * assertEqual($1, $2)
+#     $1: Output
+#     $2: Expected
+#
+#     Assert that a given output string is equal to an expected string.
+
+# * assertNotEqual($1, $2)
+#     $1: Output
+#     $2: Expected
+#
+#     Assert that a given output string is not equal to an expected
+#     string.
+
+# * assertStartsWith($1, $2)
+#     $1: Output
+#     $2: Expected
+#
+#     Assert that a given output string starts with an expected string.
+
+# * assertReturn($1, $2)
+#     $1: Output
+#     $2: Expected
+#     $?: Provided
+#
+#     Assert that the last command's return code is equal to an expected
+#     integer.
+
+# * assertNotReturn($1, $2)
+#     $1: Output
+#     $2: Expected
+#     $?: Provided
+#
+#     Assert that the last command's return code is not equal to an
+#     expected integer.
+
+# * skip()
+#
+#     Skip the current test case.
 
 ########################################################################
 # GLOBALS
